4a4d17c29ca2e2589e9bb0a923b412aef0252722
SerhiiHryhorenkoBotsCrew <serhii.hryhorenko@botscrew.com>
2024-03-12 12:48:49 +0200

    Update script for inserting common head tag

--- common-head-pages/common_head.js
+++ common-head-pages/common_head.js
@@ -1,10 +1,16 @@
+function addCommonHead() {
+  const scriptTag = document.createElement("script");
+  scriptTag.defer = true;
+  scriptTag.type = "text/javascript";
+  scriptTag.id = "chatbot-initials-script";
+  scriptTag.src = "https://chat-bbl.noform.ai/script-chatbot.js";
+  scriptTag.setAttribute(
+    "data-server-url",
+    "https://noform-ai-admin-git-nfa-468-support-widge-46647b-hembara-s-team.vercel.app"
+  );
+  scriptTag.setAttribute("data-bot-id", "659fcaa4d103a5bbb86a52ad");
 
-export function addCommonHead() {
-    const headContent = `
-    <script defer type="text/javascript" id="chatbot-initials-script" src="https://chat-bbl.noform.ai/script-chatbot.js"
-    data-server-url="https://noform-ai-admin-git-nfa-468-support-widge-46647b-hembara-s-team.vercel.app"
-    data-bot-id="659fcaa4d103a5bbb86a52ad"></script>
-    `;
-    document.head.innerHTML += headContent;
+  document.head.prepend(scriptTag);
 }
 
+document.addEventListener("DOMContentLoaded", addCommonHead);
